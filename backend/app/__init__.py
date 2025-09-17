f9ed7de95bc05b270e47fb6b3112c0084cc64b37
Lucas Lima <104467856+lucasblima-dev@users.noreply.github.com>
2025-09-17 00:08:25 -0300

    Update __init__.py
    fix: att lista de CORS em prod

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -12,6 +12,7 @@ def create_app():
     origins = [
         "http://localhost",
         "http://localhost:5173",
+        "https://autou-frontend-fkvu.onrender.com/"
     ]
 
     app.add_middleware(
